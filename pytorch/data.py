63894cb2faf2fa48812575b833294dfc45c4c8f5
Kimerth <motfoleas@yahoo.com>
2021-09-13 11:20:56 +0300

    Implemented data loading

--- pytorch/data.py
+++ pytorch/data.py
@@ -1,15 +1,51 @@
+from glob import glob
+from os.path import dirname
 from typing import Dict, List, Tuple
-import torch
-from omegaconf import DictConfig, OmegaConf
-import torchio as tio
 
-def create_data_map(cfg : DictConfig) -> Dict[str, List[str]]:
-    pass
+from omegaconf import DictConfig
+from torchio import LabelMap, ScalarImage, Subject, SubjectsDataset, Queue
+from torchio.data import UniformSampler
+from torchio.transforms import (Compose, CropOrPad, OneOf, RandomAffine,
+                                RandomBiasField, RandomElasticDeformation,
+                                RandomFlip, RandomMotion, RandomNoise,
+                                ToCanonical, ZNormalization)
 
-def load_base_data(data_map: Dict[str, List[str]]) -> Tuple[List[tio.ScalarImage], List[tio.LabelMap]]:
-    images: List[tio.ScalarImage] = []
-    labels: List[tio.LabelMap] = []
-    for path, label_paths in data_map:
-        images.append(tio.ScalarImage(path))
-        labels.append(tio.LabelMap(*label_paths))
-    return images
+
+def _create_data_map(cfg: DictConfig) -> Dict[str, List[str]]:
+    data_paths: List[str] = glob(
+        cfg["data"]["base_path"] + cfg["data"]["scan_pattern"] + cfg["data"]["file_name"], recursive=True)
+
+    return {image_path: glob(f'${image_path}/${dirname(image_path)}/*') for image_path in data_paths}
+
+
+def _load_base_data(data_map: Dict[str, List[str]]) -> List[Subject]:
+    return [Subject(ScalarImage(path), LabelMap(*label_paths)) for path, label_paths in data_map]
+
+
+def _get_transform(cfg: DictConfig):
+    return Compose([
+        # ToCanonical(),
+        CropOrPad((cfg["hparams"]["crop_or_pad_size"]),
+                  padding_mode='reflect'),
+        # RandomMotion(),
+        RandomBiasField(),
+        ZNormalization(),
+        RandomNoise(),
+        RandomFlip(axes=(0,)),
+        OneOf({
+            RandomAffine(): 0.8,
+            RandomElasticDeformation(): 0.2,
+        }), ])
+
+
+def load_dataset(cfg: DictConfig) -> Queue:
+    data_map = _create_data_map(cfg)
+
+    subjects = _load_base_data(data_map)
+    transform = _get_transform(cfg)
+    dataset = SubjectsDataset(subjects, transform)
+
+    return Queue(dataset,
+                 cfg["hparams"]["queue_length"],
+                 cfg["hparams"]["samples_per_volume"],
+                 UniformSampler(cfg["hparams"]["patch_size"]))
