05ab493516444b530abd4b5f271b58b9ed95bc82
Kimerth <motfoleas@yahoo.com>
2022-06-27 09:12:43 +0300

    Removed unnecessary code

--- VolumeRendering/source/Volume.cpp
+++ VolumeRendering/source/Volume.cpp
@@ -48,8 +48,6 @@ void Volume::load(const char* path)
         glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
         volumeData = readVolume(path, size, xtoi);
         int maxSize = std::max({ size.x, size.y, size.z });
         sizeCorrection.x = (float)size.x / maxSize, sizeCorrection.y = (float)size.y / maxSize, sizeCorrection.z = (float)size.z / maxSize;
@@ -223,8 +221,6 @@ void Volume::loadTF(float data[])
         glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
     }
 
     glBindTexture(GL_TEXTURE_1D, tfID);
